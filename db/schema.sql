430753d01254c87e906a381713ca034b018a84a6
amata7 <adrianmata@live.com>
2021-04-22 21:26:39 -0700

    created database and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,33 @@
--- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
-
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE category (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_name VARCHAR(30) NOT NULL,
+  price DEC NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT
+);
+
+CREATE TABLE tag (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  tag_name VARCHAR(30)
+);
+
+CREATE TABLE product_tag (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  tag_id INT
+);
+
+SELECT * FROM category;
+SELECT * FROM product;
+SELECT * FROM tag;
+SELECT * FROM product_tag;
