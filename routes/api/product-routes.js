7060edbc0d7f93c1939c8a304d4ed91409886bb7
amata7 <adrianmata@live.com>
2021-04-24 22:56:16 -0700

    added GET route for all tags, updated products routes to include Category aand Tag data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,8 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
-    });
+    const productData = await Product.findAll(
+      {
+        include: [{ model: Product, Category }],
+      });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -22,6 +24,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Product, Category }],
     });
 
     if (!productData) {
