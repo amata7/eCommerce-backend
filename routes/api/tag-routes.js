6b7bfc84d71b0623a7ce99fc067d62a72ccb9616
amata7 <adrianmata@live.com>
2021-04-25 16:03:44 -0700

    added POST and PUT routes to tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,10 +37,34 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json({ message: 'Tag created' });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        }
+      },
+      );
+
+    res.status(200).json({ message: 'Tag updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
